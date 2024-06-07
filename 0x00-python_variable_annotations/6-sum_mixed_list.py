ab7cf1f17d5fd7fe4bf0fd720ff2e0f3daab56e1
Travis <ondegotravis@gmail.com>
2024-06-07 23:32:20 +0300

    Correct typing imports

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """type annotated function that takes float and int
 returns sum as a float"""
-from typing import Dict
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: Dict[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Returns sum of list as a float"""
     return sum(mxd_lst)
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,6 +4,6 @@ hat takes a string k and an int OR float v as arguments and returns a tuple"""
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple of string and a float"""
     return (k, float(v**2))
